20d15a02a1ee3a1af4b7d90591ab1c0177167125
IbrahimZahidd <buttibrahim848@.com>
2025-03-20 05:35:03 +0500

    Your commit message here

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  content: ["./index.html", "./index2.html", "./index3.html", "./index4.html"],
+  content: ["./index.html", "./index2.html", "./index3.html", "./index4.html",],
   plugins: {
     "@tailwindcss/postcss": {},
   },
+  // // // Disable filter plugin
+  // corePlugins: {
+  //   filter: false,
+  // },
 };
